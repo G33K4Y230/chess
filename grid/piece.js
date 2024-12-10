64f7c50c82b8f4ef22a2a7518d00296cc65f9e46
G33K4Y230 <gkandandji@gmail.com>
2024-12-10 17:15:21 +0000

    add click

--- grid/piece.js
+++ grid/piece.js
@@ -17,23 +17,26 @@ const generatePieces = () => {
 class Piece {
 
     constructor(type, x, y, player) {
-        this._type = type
+        this._id = "piece-" + type + "-" + x + "-" + y + "-" + player;
+        this._type = type;
         this._x = x;
         this._y = y;
-        this._player = player
+        this._player = player;
         spaces.forEach((space) => {
             if(space.x == this._x && space.y == this._y) {
                 space.piece = this
-                return
             }
         })
         pieces.push(this)
     }
 
+    get id() { return this._id }
     get x() { return this._x }
     get y() { return this._y }
     get type() { return this._type }
     get player() { return this._player }
+
+    set id(id) { this._id = id }
 
     set x(x) {
         if(x > 0 && x <= 8) this._x = x
@@ -54,10 +57,10 @@ class Piece {
 
     generateImage() { 
         return "<img "
-            + "src=\"img/chess-" + this._type + "-svgrepo-com" + (this._player == "white" ? "-invert" : "") + ".svg\""
+            + "id=\"" + this._id + "\" "
+            + "src=\"img/chess-" + this._type + "-svgrepo-com" + (this._player == "white" ? "-invert" : "") + ".svg\" "
             + "class=\""
             + "max-w-[70%]" + " "
-            + "stroke-cyan-500" + " "
             + "\""
             + "/>"
       }
